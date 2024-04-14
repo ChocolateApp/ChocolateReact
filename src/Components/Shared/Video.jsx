674e1dcdd8f44f6976dcf4a411322d682ed7ffbc
Impre-visible <impr.visible@gmail.com>
2024-04-14 13:10:54 +0200

    ✨ Feat: Added subtitles.

--- src/Components/Shared/Video.jsx
+++ src/Components/Shared/Video.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
-import { IoPause, IoPlay, IoVolumeMute, IoVolumeHigh, IoVolumeLow, IoVolumeMedium } from 'react-icons/io5';
+import { IoPause, IoPlay, IoVolumeMute, IoVolumeHigh, IoVolumeLow, IoVolumeMedium, IoCheckmark } from 'react-icons/io5';
 import { MdFullscreen, MdFullscreenExit, MdPictureInPicture, MdPictureInPictureAlt, MdSubtitles } from 'react-icons/md'; // eslint-disable-line no-unused-vars
 import { useNavigate } from 'react-router-dom';
 
@@ -26,7 +26,11 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
   const [skipButtonTime, setSkipButtonTime] = useState(0);
   const [skipButtonType, setSkipButtonType] = useState('intro');
   const [timeOfPreviousClick, setTimeOfPreviousClick] = useState(0);// eslint-disable-line no-unused-vars
-  const [timeOfPreviousMouseMovement, setTimeOfPreviousMouseMovement] = useState(0);// eslint-disable-line no-unused-vars
+  const [timeOfPreviousMouseMovement, setTimeOfPreviousMouseMovement] = useState(0);
+  const [subtitlesOpen, setSubtitlesOpen] = useState(false);
+  const [captions, setCaptions] = useState([]);
+  const [selectedCaption, setSelectedCaption] = useState(null);
+  const [currentCaption, setCurrentCaption] = useState(null);
 
 
   const [volumeIcon, setVolumeIcon] = useState(<IoVolumeHigh className="video-player-volume-icon player-control-icon" />);
@@ -48,6 +52,28 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
     } else {
       return `00:00:${ss}`;
     }
+  }
+
+  function convertTime(timeString) {
+    const timeComponents = timeString.split(':');
+    let seconds = 0;
+
+    // Si le temps est au format hh:mm:ss.ms
+    if (timeComponents.length === 3) {
+      seconds += parseInt(timeComponents[0]) * 3600; // heures en secondes
+      seconds += parseInt(timeComponents[1]) * 60;   // minutes en secondes
+    } else if (timeComponents.length === 2) { // Si le temps est au format mm:ss.ms
+      seconds += parseInt(timeComponents[0]) * 60;   // minutes en secondes
+    }
+
+    // Ajoute les secondes et les millisecondes
+    const lastComponent = timeComponents[timeComponents.length - 1].split('.');
+    seconds += parseInt(lastComponent[0]); // secondes
+    if (lastComponent.length > 1) {
+      seconds += parseFloat("0." + lastComponent[1]); // millisecondes
+    }
+
+    return seconds;
   }
 
   function handleVolumeChange(e) {
@@ -178,21 +204,116 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
   }
 
   const handleVideoStream = (duration) => {
+    //remove duplication in captions
+    setCaptions(prevCaptions => [...new Set(prevCaptions.map(JSON.stringify))].map(JSON.parse));
+    console.log(captions);
+    console.log(selectedCaption);
     setProgress(duration);
     checkSkipButton();
+
+    if (!selectedCaption) return;
+
+    let currentTime = duration;
+    let segments = selectedCaption.segments;
+    for (let i = 0; i < segments.length; i++) {
+      let start = convertTime(segments[i].start);
+      let end = convertTime(segments[i].end);
+      let text = segments[i].text;
+      console.log(start, end, text);
+      if (currentTime >= start && currentTime <= end) {
+        setCurrentCaption(text);
+        console.log(text);
+        return;
+      } else if (currentTime < start) {
+        setCurrentCaption(null);
+        return;
+      }
+    }
+    setCurrentCaption(null);
   }
 
   useEffect(() => {
-    let volume = localStorage.getItem('volume');
-    if (volume) {
-      volume = parseFloat(volume);
-      setVolume(volume);
-      handleVolumeChange({ target: { value: volume } });
-    }
-    setIsPlaying(true);
-    setIsPlaying(false);
-    setIsPlaying(true);
+    const fetchVolume = () => {
+      const volume = parseFloat(localStorage.getItem('volume'));
+      if (!isNaN(volume)) {
+        setVolume(volume);
+        handleVolumeChange({ target: { value: volume } });
+      }
+    };
+
+    const fetchSubtitles = async () => {
+      try {
+        const video_url = options.sources[0].src;
+        const parent_url = video_url.split('/').slice(0, -2).join('/');
+        const response = await fetch(video_url, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        const data = await response.text();
+        const m3u8_file = data.split('\n');
+
+        for (let index in m3u8_file) {
+          const line = m3u8_file[index];
+          if (line.includes('#EXT-X-MEDIA:TYPE=SUBTITLES')) {
+            const caption_data = {};
+            caption_data['id'] = line.split('URI="')[1].split('"')[0].split("_")[2].split('.')[0];
+            caption_data['name'] = line.split('NAME="')[1].split('"')[0];
+            caption_data['language'] = line.split('LANGUAGE="')[1].split('"')[0];
+            caption_data['segments'] = [];
+            const args = line.split(',');
+            const uri = parent_url + args[5].split('"')[1];
+            const response = await fetch(uri, {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+              }
+            });
+            const vtt_data = await response.text();
+            const vtt_file = vtt_data.split('\n')[vtt_data.split('\n').length - 3];
+            const vtt_uri = parent_url + vtt_file;
+            const vtt_response = await fetch(vtt_uri, {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+              }
+            });
+            const vtt_text = await vtt_response.text();
+            const lines = vtt_text.split('\n');
+            let index = 0;
+            while (index < lines.length) {
+              if (lines[index] === 'WEBVTT') {
+                index++;
+                continue;
+              }
+              if (lines[index].includes("-->")) {
+                let line_index = index + 1;
+                let text = "";
+                while (line_index < lines.length && lines[line_index] !== "") {
+                  text += lines[line_index] + "\n";
+                  line_index++;
+                }
+                const start = lines[index].split(' --> ')[0];
+                const end = lines[index].split(' --> ')[1];
+                caption_data['segments'].push({ start: start, end: end, text: text });
+                index = line_index;
+              }
+              index++;
+            }
+            //check if the caption already exists
+            setCaptions(prevCaptions => [...prevCaptions, caption_data]);
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching subtitles:', error);
+      }
+    };
+
+    fetchVolume();
+    fetchSubtitles();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
 
   const handleVideoClick = () => {
     setTimeOfPreviousClick(prevTime => {
@@ -222,17 +343,29 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
     setTimeOfPreviousMouseMovement(new Date().getTime());
   };
 
+  const handleCaptionSelection = (caption) => {
+    console.log(caption);
+    console.log(selectedCaption);
+    if (selectedCaption && caption && selectedCaption.id === caption.id) {
+      setSelectedCaption(null);
+      setCurrentCaption(null);
+      return;
+    }
+    setSelectedCaption(caption);
+    setSubtitlesOpen(false);
+  };
+
   useEffect(() => {
     //check every second, if the mouse haven't moved in the last 5 seconds, add 'disabled' class to the player controls
     const interval = setInterval(() => {
       //console.log(`Time of previous mouse movement: ${timeOfPreviousMouseMovement}`);
       //console.log(`Spent time since last mouse movement: ${new Date().getTime() - timeOfPreviousMouseMovement}`);
       if (new Date().getTime() - timeOfPreviousMouseMovement > 5000) {
+        if (playerControls.current.classList.contains('disabled')) return;
         playerControls.current.classList.add('disabled');
-        console.log('added class');
+        setSubtitlesOpen(false);
       } else {
         playerControls.current.classList.remove('disabled');
-        console.log('removed class');
       }
     });
     return () => clearInterval(interval);
@@ -280,7 +413,9 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
           }}
         />
         <div className='player-overlay'>
-          <div className='player-subtitle'></div>
+          <div className='player-subtitle'>
+            <p>{currentCaption}</p>
+          </div>
           <div className='player-loader'>
             {isLoading && <div className="spinner"></div>}
           </div>
@@ -317,16 +452,19 @@ export const Video = ({ options, onReady = () => { }, previousURL = null, previo
                 <span>/</span>
                 <span>{formatTime(duration)}</span>
               </div>
-              {/*
               <div className='player-subtitles-popup' style={{ opacity: subtitlesOpen ? '1' : '0', pointerEvents: subtitlesOpen ? 'all' : 'none' }}>
-                <p>Test</p>
-                <p>Test</p>
-                <p>Test</p>
+                {captions.map((caption, index) => {
+                  return (
+                    <div key={index} className='player-caption' onClick={() => handleCaptionSelection(caption)}>
+                      {(selectedCaption && selectedCaption.id === caption.id) ? <IoCheckmark className='player-control-icon' /> : <div></div>}
+                      <h3>{caption.name} ({caption.language})</h3>
+                    </div>
+                  );
+                })}
               </div>
               <div className='player-subtitles-selector'>
                 <MdSubtitles className='player-control-icon' onClick={() => setSubtitlesOpen(!subtitlesOpen)} />
               </div>
-              */}
               <div className='player-pip'>
                 {pip ? (
                   <MdPictureInPicture
