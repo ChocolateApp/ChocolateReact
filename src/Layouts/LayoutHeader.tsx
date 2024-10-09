ed81d448d4f1768b7f37f9bda5408747fac7d5d7
Impre-visible <impr.visible@gmail.com>
2024-10-09 18:18:17 +0200

    Refactor: Removed check calls (cause useFetch does that)

--- src/Layouts/LayoutHeader.tsx
+++ src/Layouts/LayoutHeader.tsx
@@ -1,24 +1,7 @@
 import React from 'react';
 import Header from '@/Components/Header/Header';
-import { usePost } from '@/Hooks/useFetch';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 const LayoutHeader = ({ children }: { children?: React.ReactNode }) => {
-
-    const { handleSubmit } = usePost();
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    React.useEffect(() => {
-        handleSubmit({
-            url: '/api/auth/check',
-            dispatcher: (data: any) => {
-                if (data.code === 243) {
-                    navigate('/login');
-                }
-            }
-        });
-    }, [location.pathname]);
 
     return (
         <>
