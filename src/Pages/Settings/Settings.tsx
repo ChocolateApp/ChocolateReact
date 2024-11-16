aa26746c19a6dcb95cd32786d194dda58f4cd9b8
Impre-visible <impr.visible@gmail.com>
2024-11-16 18:46:19 +0100

    ✨ feat: Implemented Settings

---
+++ src/Pages/Settings/Settings.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import GeneralSettings from './Subpages/GeneralSettings';
+import AccountsSettings from './Subpages/AccountsSettings';
+
+const Settings: React.FC = () => {
+    const [currentSection, setCurrentSection] = useState<string>(window.location.hash || '#general');
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            setCurrentSection(window.location.hash || '#general');
+        };
+
+        window.addEventListener('hashchange', handleHashChange);
+
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        };
+    }, []);
+
+    return (
+        <div className="flex h-screen pt-20">
+            <div className="w-1/6 border-r-2 border-zinc-800 text-white p-4">
+                <h2 className="text-xl font-bold mb-4">Settings</h2>
+                <ul>
+                    <li className="mb-2">
+                        <a href="#general" className={`hover:underline ${currentSection === '#general' ? 'font-bold' : ''}`}>General</a>
+                    </li>
+                    <li className="mb-2">
+                        <a href="#accounts" className={`hover:underline ${currentSection === '#accounts' ? 'font-bold' : ''}`}>Accounts</a>
+                    </li>
+                    <li className="mb-2">
+                        <a href="#libraries" className={`hover:underline ${currentSection === '#libraries' ? 'font-bold' : ''}`}>Libraries</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="w-5/6 p-4 overflow-y-scroll">
+                {currentSection === '#general' && (
+                    <GeneralSettings />
+                )}
+                {currentSection === '#accounts' && (
+                    <AccountsSettings />
+                )}
+                {currentSection === '#libraries' && (
+                    <div>
+                        <h2 className="text-xl font-bold mb-4">Libraries</h2>
+                        <p>Libraries settings</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Settings;
