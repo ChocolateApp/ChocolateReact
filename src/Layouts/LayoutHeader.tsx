c15dc650f586dbfbfdb1a3b1c032192f22e1a178
Impre-visible <impr.visible@gmail.com>
2024-11-17 10:57:27 +0100

    ✨ feat: Redirect to libraries settings if no libraries are found

--- src/Layouts/LayoutHeader.tsx
+++ src/Layouts/LayoutHeader.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@/Components/Header/Header';
+import { useGet } from '@/Hooks/useFetch';
+import { useNavigate } from 'react-router-dom';
 
 const LayoutHeader = ({ children }: { children?: React.ReactNode }) => {
+
+    const { data: librariesData } = useGet('/api/settings/libraries');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (librariesData && librariesData.data.length == 0) {
+            navigate('/settings#libraries');
+        }
+    }, [librariesData]);
 
     return (
         <>
